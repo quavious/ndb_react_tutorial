22a4b50654a294b445e134a1e830a764cebca873
quavious <leenowon97@gmail.com>
2020-10-16 23:52:58 +0900

    Create Customer DB Table

--- server.js
+++ server.js
@@ -1,39 +1,30 @@
 import express from "express"
 import bodyParser from "body-parser";
+import mysql from "mysql";
 
+import dbKey from "./database.js";
 const app = express()
 const port = 5000;
+
+const dbInfo = dbKey();
+const connection = mysql.createConnection({
+    host: dbInfo.host,
+    user: dbInfo.user,
+    password: dbInfo.password,
+    port: dbInfo.port,
+    database: dbInfo.database,
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/api/customers", (req, res) => {
-    res.send([
-        {
-            id: 1,
-            image: "https://placeimg.com/64/64/1",
-            name: "홍길동",
-            birth: "961222",
-            gender: "남자",
-            job: "대학생"
-        },
-        {
-            id: 2,
-            image: "https://placeimg.com/64/64/2",
-            name: "김공익",
-            birth: "961222",
-            gender: "남자",
-            job: "대학생"
-        },
-        {
-            id: 3,
-            image: "https://placeimg.com/64/64/3",
-            name: "이공군",
-            birth: "991220",
-            gender: "남자",
-            job: "부사관"
-        },
-    ])
+    connection.query("select * from customer", (err, rows, fields) => {
+        res.send(rows);
+    })
 })
 
-app.listen(port, () => console.log("Server On 5000"))
+app.listen(port, () => {
+    connection.connect()
+    console.log("Server On 5000")
+})
